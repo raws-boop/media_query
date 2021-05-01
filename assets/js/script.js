a0831cab32abcf79add08467479f8d4005e47fd5
Ross Holtrop <holtrop.ross@gmail.com>
2021-05-01 12:41:42 -0700

    updating js with second api git push

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,19 @@ fetch("https://streaming-availability.p.rapidapi.com/search/basic?country=us&ser
 .catch(err => {
 	console.error(err);
 });
+
+
+//Movie Database *use for search*
+fetch("https://movie-database-imdb-alternative.p.rapidapi.com/?i=tt4154796&r=json", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "cb566b1c57msh5161a956c5815e1p162df6jsn46969de8ab19",
+		"x-rapidapi-host": "movie-database-imdb-alternative.p.rapidapi.com"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
