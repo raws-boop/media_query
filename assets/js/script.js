cb27e047aaad09889f6a0fdd48cc5ca9802ed4ab
DrewG <80792838+drewg07@users.noreply.github.com>
2021-05-06 23:16:05 -0700

    updated query button fuctionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 var searchResultsEl = document.querySelector("#searchResults")
 var searchEl = document.querySelector("#searchForm")
-// var searchBtnEl = document.querySelector("#inputButton")
+var searchBtnEl = document.querySelector("#queryBtn")
 
 function displayResults(event){
 	event.preventDefault();
@@ -78,4 +78,73 @@ function searchMovieOrTv(inputSearchVal) {
 //     console.log(data);
 // })
 
+
+function displayBtnResults(event){
+	event.preventDefault();
+	console.log("queryCheck")
+	var selectedService = document.querySelector("input[name='service']:checked")
+		console.log("selectedService",selectedService.value)
+	var selectedGenre = document.querySelector("input[name='genre']:checked")
+		console.log("selectedGenre",selectedGenre.value)
+	var selectedMediaType = document.querySelector("input[name='media']:checked")
+		console.log("selectedMediaType",selectedMediaType.value)
+
+	searchStreamingService(selectedService.value, selectedGenre.value, selectedMediaType.value);
+}
+
+function searchStreamingService(selectedService, selectedGenre, selectedMediaType) {
+
+    // Streaming Availability API endpoints
+    fetch("https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=" + selectedService + "&type=" + selectedMediaType + "&genre=" + selectedGenre + "&page=1&language=en", {
+        method: "GET",
+        headers: {
+            "x-rapidapi-key": "cb566b1c57msh5161a956c5815e1p162df6jsn46969de8ab19",
+            "x-rapidapi-host": "streaming-availability.p.rapidapi.com"
+        }
+    })
+    // if (checkBox.checked == true){
+    //     text.style.display = "block";
+    // } else {
+    //     text.style.display = "none";
+    // }
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+	displayQueryResults(data)
+    //call an additional function to display results
+    })
+    .catch(err => {
+    //console.error(err);
+    })
+        console.log("post fetch");
+}
+function displayQueryResults(dataResults) {
+
+}
+function initGenreCheckboxes() {
+
+    // Streaming Availability API endpoints
+    fetch("https://streaming-availability.p.rapidapi.com/genres", {
+        method: "GET",
+        headers: {
+            "x-rapidapi-key": "cb566b1c57msh5161a956c5815e1p162df6jsn46969de8ab19",
+            "x-rapidapi-host": "streaming-availability.p.rapidapi.com"
+        }
+    })
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    //call an additional function to display results
+    })
+    .catch(err => {
+    //console.error(err);
+    })
+        console.log("post fetch");
+}
+
 searchEl.addEventListener('submit', displayResults)
+searchBtnEl.addEventListener('click', displayBtnResults)
