e6c90effeadcf9dbbdba6a8269c24d19a8057639
MikeAdu <mike_adusah@yahoo.com>
2021-05-07 20:19:45 -0700

    local storage

---
+++ assets/js/local.js
@@ -0,0 +1,26 @@
+
+  var icon=document.getElementById("icon");
+  // Initialize a variable that will look for 'isDarkMode' in local storage, if it is undefined, defaut to false
+  let isDarkmode = window.localStorage.getItem('isDarkmode') || false;
+  console.log(isDarkmode)
+
+  // separate the onclick to be able to toggle our variable
+  icon.onclick=function(){
+    isDarkmode = !isDarkmode
+    localStorage.setItem("isDarkmode", isDarkmode)
+    isDarkModeFunction();
+  }
+
+  //check the value of is dark mode and add or removes the classlist
+  function isDarkModeFunction() {
+    if(isDarkmode){
+      document.body.classList.add("dark-theme")
+      icon.src= "assets/images/sun.png";
+    } else {
+      document.body.classList.remove("dark-theme")
+      icon.src= "assets/images/moon.png";
+    }
+  }
+
+  // call the function upon document load
+  isDarkModeFunction();
